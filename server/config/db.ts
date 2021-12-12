87d26b373c70378275ffa073246b672d602f2f89
ChogLight <90294589+ChogLight@users.noreply.github.com>
2021-12-11 20:01:56 -0500

    Delete db.ts

--- server/config/db.ts
+++
@@ -1,4 +0,0 @@
-export const LocalURI = 'mongodb://localhost:27017/portfolio';
-export const RemoteURI = 'mongodb+srv://chog:mIGgJ4KMrBhMc7TT@cluster0.uy5cy.mongodb.net/portfolio?retryWrites=true&w=majority';
-export const Secret = "HelloTeacher";
-export const HostName = "localhost";
